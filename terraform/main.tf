91e109ede42d3bc1e8b260231f7c62f22614e7f6
KumarSankaran, Kranthi <kranthi-kumar.sankaran@sakscloudservices.com>
2025-04-04 13:07:52 +0530

    77).Deploying to ec2 in CI/CD to github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,8 +142,7 @@ resource "aws_iam_role" "ec2_role" {
   })
 
   lifecycle {
-    # Prevent destroy of existing role
-    prevent_destroy = true
+    create_before_destroy = true
   }
 }
 
@@ -155,6 +154,10 @@ resource "aws_iam_role_policy_attachment" "s3_access" {
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project_name}-ec2-profile"
   role = aws_iam_role.ec2_role.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # EC2 Instance
